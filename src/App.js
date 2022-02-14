5e70f0d20d8f3a424aaeddabb86c229c4e750460
Sharad Agarwal <obviouslysharad@gmail.com>
2022-02-15 01:14:39 +0530

    Implemented wallpapers and other tags

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import ShowerThoughts from "./components/ShowerThoughts";
 import Header from "./components/Header";
 import Tags from "./components/Tags";
 import BeautifulData from "./components/BeautifulData";
+import BlackMagic from "./components/BlackMagic";
+import Memes from "./components/Memes";
+import Wallpapers from "./components/Wallpapers";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Tags />
       <ShowerThoughts />
       <BeautifulData />
+      <BlackMagic />
+      <Memes />
+      <Wallpapers />
     </div>
   );
 }
